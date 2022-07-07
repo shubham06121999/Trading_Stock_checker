cafc0b37500b79f291a25a87fc2e7239ecb2fee6
spartan app solutions <sdubey@spartanappsolutions.com>
2022-07-07 18:22:32 +0530

    setup for java Spring

---
+++ TradeChecker/src/main/java/com/example/TradeChecker/TradeCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.example.TradeChecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeCheckerApplication.class, args);
+	}
+
+}
